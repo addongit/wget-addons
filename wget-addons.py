b40319b3d468ff480bff96fe33dee38a9e2c3eb3
Robert Jackson <rmjackson@gmail.com>
2011-04-30 21:00:04 -0700

    fixed and working. --reset-master option added that must be called during a common profile update. Commented out zip archiving for the time being until I work out a better solution.
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

--- wget-addons.py
+++ wget-addons.py
@@ -15,42 +15,69 @@ from zipfile import ZipFile
 def url2name(url):
   return basename(urlsplit(url)[2])
 
-def download(url, localFileName = None):
-  localName = url2name(url)
+def download(url, override_filename=None):
+  local_name = url2name(url)
   req = urllib2.Request(url)
   r = urllib2.urlopen(req)
+  
   if r.info().has_key('Content-Disposition'):
     # If the response has Content-Disposition, we take file name from it
-    localName = r.info()['Content-Disposition'].split('filename=')[1]
-    if localName[0] == '"' or localName[0] == "'":
-      localName = localName[1:-1]
+    local_name = r.info()['Content-Disposition'].split('filename=')[1]
+    if local_name[0] == '"' or local_name[0] == "'": local_name = local_name[1:-1]
   elif r.url != url: 
     # if we were redirected, the real file name we take from the final URL
-    localName = url2name(r.url)
-  if localFileName: 
-    # we can force to save the file as specified name
-    localName = localFileName
-  f = open(localName, 'wb')
+    local_name = url2name(r.url)
+
+  filename_on_disk = override_filename if not override_filename is None else local_name
+  
+  f = open(filename_on_disk, 'wb')
   f.write(r.read())
   f.close()
+  return filename_on_disk
+
 
 def unzip(path, archive=False):
   if not os.path.exists(path): raise Exception('%s does not exist.' % path)
   os.chdir(path)
+  
+  processed_files = { 'archived':   [],
+                      'extracted':  []
+                    }
+
   for f in glob.glob('*.zip'):
     with ZipFile(f, 'r') as z:
       z.extractall()
-  if archive: archive_zips(path)
+  #if archive: processed_files['archived'] = archive_zips(path)
+  clean_up_zips(path)
+  for d in os.listdir(path):
+    if d == '_archived': continue
+    processed_files['extracted'].append(os.path.join(path, d))
+  return processed_files
 
 def archive_zips(path):
   archive_path = os.path.join(path, '_archive')
   if not os.path.exists(path): raise Exception('%s does not exist.' % path)
   os.chdir(path)
+  archived_files = []
   if not os.path.exists(archive_path): os.makedirs(archive_path)
   for f in glob.glob('*.zip'):
-    print 'Archiving: %s' % f
-    shutil.move( os.path.join(path, f), os.path.join(archive_path, f) )
+    archive_file_src_path = os.path.join(path, f)
+    archive_file_dst_path = os.path.join(archive_path, f)
+    if os.path.exists(archive_file_dst_path): os.remove(archive_file_dst_path)
+    shutil.move( archive_file_src_path, archive_file_dst_path )
+    archived_files.append(archive_file_dst_path)
+  return archived_files
 
+
+def clean_up_zips(path):
+  if not os.path.exists(path): raise Exception('%s does not exist.')
+  cwd = os.getcwd()
+  os.chdir(path)
+  for f in glob.glob('*.zip'): 
+    zip_path = os.path.join(path, f)
+    os.remove(zip_path)
+  os.chdir(cwd)
+  
 
 def initConfig(config):
   ''' Initialize the config object pointed to $config's path.'''
@@ -74,12 +101,10 @@ def find_all_profiles(profiles_path, profile=None):
 
 
 def get_addons_list(profile):
-  print sys.path[0]
   if not os.path.exists(profile): raise Exception('%s does not exist motherbitches' % profile)
   addons = []
   for a in open(profile).readlines():
     if ';' in a or a is None or len(a) == 0 or a == '\n': continue
-    print 'line: %s' % a
     addons.append(a.strip())
   return addons
 
@@ -98,7 +123,8 @@ def Main(opts):
                    'depot_folder':    opts['depot_folder'] if opts['depot_folder'] else config.get('local', 'depotDirectory'),
                    'extract':         opts['extract'],
                    'archive':         opts['archive'],
-                   'merge_common':    opts['common']
+                   'merge_common':    opts['common'],
+                   'reset_master':    opts['reset_master']
                  }
 
   verbose = app_settings['verbose']
@@ -117,21 +143,30 @@ def Main(opts):
       glob_profile = find_all_profiles( app_settings['profiles_folder'], p )
       if len(glob_profile) == 0: continue
       addons_profiles.append(glob_profile[0]) 
-  print addons_profiles
+  if verbose: print 'Using the following profiles:\n\t%s' % '\n'.join(addons_profiles)
 
   cwd = os.getcwd()
   os.chdir( app_settings['depot_folder'] )
   
   master_path = os.path.join(app_settings['depot_folder'], 'master')
+  #master_archive_path = os.path.join(master_path, '_archive')
 
-  if not os.path.exists(master_path): os.makedirs(master_path)
+  #if not os.path.exists(master_path): os.makedirs(master_path)
+  #if not os.path.exists(master_archive_path): os.makedirs(master_archive_path)
 
   for profile in addons_profiles:
-    full_depot_profile = os.path.join( app_settings['depot_folder'], profile.split('.')[0] )
+    profile_name = profile.split('.')[0]
+    full_depot_profile = os.path.join( app_settings['depot_folder'], profile_name )
     
+    if os.path.exists(full_depot_profile):
+      if verbose: print '(%s) Cleaning current addon collection.' % profile_name
+      shutil.rmtree(full_depot_profile)
+
     if not os.path.exists( full_depot_profile ): 
+      if verbose: print '(%s)\tCreating %s' % (profile_name, full_depot_profile)
+      
       if dry_run:
-        print 'mkdir %s' % full_depot_profile
+        pass
       else:
         os.makedirs( full_depot_profile )
     
@@ -139,31 +174,67 @@ def Main(opts):
     
     profile_full_path = os.path.join(app_settings['profiles_folder'], profile)
     
-    for a in get_addons_list( profile_full_path ):
+    if verbose: print '(%s)\tprofile_full_path: %s' % (profile_name, profile_full_path)
+    
+    for addon_url in get_addons_list( profile_full_path ):
+      if verbose: print '(%s)\tDownloading %s' % (profile_name, addon_url)
       if dry_run: 
-        print 'download %s' % a
+        pass
       else:
-        download(a)
-        # hacking master in
-        os.chdir(master_path)
-        download(a)
-        os.chdir(full_depot_profile)
+        addon_name = download(addon_url)
 
       if app_settings['extract']: 
+        if verbose: 
+            print '(%s)\tUnzipping %s' % (profile_name, addon_name)
+            #print '(%s)\tArchive:\t%s' % ( profile_name, app_settings['archive'] )
         if dry_run:
-          print 'unzip %s\narchive=%s' % ( full_depot_profile, app_settings['archive'] )
+          pass 
         else:
-          unzip( full_depot_profile, app_settings['archive'] )
-          os.chdir(master_path)
-          unzip( master_path, app_settings['archive'] )
-          os.chdir(full_depot_profile)
+          files = unzip( full_depot_profile, app_settings['archive'] )
+
+
+    # Master cloning
+    #============================================================================
+    #archive_profile_path = os.path.join(full_depot_profile, '_archive')
+    #if verbose: print '(%s) archive_profile_path: %s' % (profile_name, archive_profile_path)
+    
+    if app_settings['reset_master'] and profile_name == 'common':
+      if verbose: print 'Resetting Master'
+      shutil.rmtree(master_path)
+
+    if not os.path.exists(master_path): os.makedirs(master_path)
+
+    for f in os.listdir(full_depot_profile):
+      if f == '_archive': continue
+      if verbose: print '(%s) Merging %s into Master' % (profile_name, f)
+      addon_src_path = os.path.join(full_depot_profile, f)
+      addon_dst_path = os.path.join(master_path, f)
+
+      if os.path.exists(addon_dst_path): shutil.rmtree(addon_dst_path)
+
+      shutil.copytree(addon_src_path, addon_dst_path)
+      
+      #if os.path.exists(archive_profile_path):
+      #  cwd = os.getcwd()
+      #  os.chdir(archive_profile_path)
+      #  for f in glob.glob('*.zip'):
+      #    zip_dst = os.path.join(master_archive_path, f)
+      #    zip_src = os.path.join(archive_profile_path, f)
+      #    if verbose: print '(Master) Cloning %s into _archive/' % (f)
+      #    if os.path.exists(zip_dst): os.remove(zip_dst)
+      #    shutil.copy( zip_src, zip_dst )
+      #  os.chdir(cwd)
+
+    if verbose: 
+      print '\n(%s) Addon listing after update:\n' % profile_name
+      for a in os.listdir('./'):
+        print '\t%s\n' % a
+      print '\n'
 
   common_addons_path = os.path.join(app_settings['depot_folder'], 'common')
   common_addons_list = os.listdir(common_addons_path)
 
-  print '--common: %s' % app_settings['merge_common']
   if len(common_addons_list) == 1 and '_archive' in common_addons_list or len(common_addons_list) == 0: app_settings['merge_common'] = False
-  print '--common: %s' % app_settings['merge_common']
 
   if app_settings['merge_common']:
     for profile in addons_profiles:
@@ -185,9 +256,6 @@ def Main(opts):
         else:
           shutil.copytree(addon_src_path, addon_dst_path)
 
-
-  print os.listdir('./')
-
   os.chdir( cwd )
 
 if __name__ == '__main__':
@@ -200,6 +268,7 @@ if __name__ == '__main__':
 
     _parser.add_option( '--profile',
                         '-p',
+                        '-b',
                         action='append',
                         type='string',
                         default=[],
@@ -239,6 +308,12 @@ if __name__ == '__main__':
                         dest='common',
                         help='Merge common addons with profiles.')
     
+    _parser.add_option('--reset-master',
+                        action='store_true',
+                        default=False,
+                        dest='reset_master',
+                        help='Reset Master addon collection.')
+    
     _parser.add_option('--addons-depot-folder',
                        action='store',
                        type='string',
@@ -270,7 +345,8 @@ if __name__ == '__main__':
              'dry_run':         _opts.dry_run, 
              'extract':         _opts.extract,
              'archive':         _opts.archive, 
-             'common':          _opts.common 
+             'common':          _opts.common, 
+             'reset_master':    _opts.reset_master 
            }
 
     return opts
